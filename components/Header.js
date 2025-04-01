1f39aec5859d163aa6d68c0c3cc33fb5eaad426e
JadeMawn <jade.mawn@richmond.edu>
2025-04-01 21:12:08 +0200

    texts

--- components/Header.js
+++ components/Header.js
@@ -14,7 +14,6 @@ export default function Header() {
     
     return () => {
       clearInterval(bounceInterval);
-      clearInterval(taglineInterval);
     };
   }, []);
   
--- next.config.js
+++ next.config.js
@@ -2,8 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [], // Add any external domains if needed
-    unoptimized: true // Add this line for development
+    domains: ['example.com'], // Add actual domains you're using
+    unoptimized: true // Skip optimization for now to speed up build
+  },
+  // For Netlify, add this to increase build memory
+  webpack: (config, { isServer }) => {
+    // Reduce chunk size
+    config.optimization.splitChunks = {
+      chunks: 'all',
+      cacheGroups: {
+        default: false,
+        vendors: false,
+      },
+    };
+    return config;
   },
 }
 
